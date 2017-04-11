e445aa4661e8f6b9a2f9dc600a1197f24cdd2a33
keitakurita <kris337jbn@yahoo.co.jp>
2017-04-11 20:44:40 +0900

    rewrote slack personal to not need slackclient

--- slack_personal.py
+++ slack_personal.py
@@ -1,12 +1,17 @@
-from slackclient import SlackClient
 import os
+import requests
 
 
 def notify_self(message):
     slack_token = os.environ["SLACK_TOKEN"]
     slack_id = os.environ["SLACK_ID"]
-    sc = SlackClient(slack_token)
-    return sc.api_call("chat.postMessage", channel="@" + slack_id, text=message)
+    parameters = {
+        "token": slack_token,
+        "channel": "@" + slack_id,
+        "text": message
+    }
+    r = requests.post("https://slack.com/api/chat.postMessage", params=parameters)
+    return r.text
 
 
 if __name__ == '__main__':
