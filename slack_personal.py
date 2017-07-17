66728799f58df5be60e095d4f53986df6fb5b87d
keitakurita <keita.kurita@gmail.com>
2017-07-17 20:39:16 +0900

    Made notify into a magic command

--- slack_personal.py
+++ slack_personal.py
@@ -1,5 +1,8 @@
 import os
 import requests
+from IPython.core import magic_arguments
+from IPython.core.magics import ExecutionMagics
+from IPython.core.magic import cell_magic, magics_class
 
 
 def notify_self(message):
@@ -14,5 +17,23 @@ def notify_self(message):
     return r.text
 
 
-if __name__ == '__main__':
-    notify_self("hello world")
+@magics_class
+class MessengerMagics(ExecutionMagics):
+
+    def __init__(self, shell):
+        super().__init__(shell)
+
+    @cell_magic
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument("message", type=str)
+    def notify(self, line="", cell=None):
+        args = magic_arguments.parse_argstring(self.notify, line)
+        try:
+            self.shell.ex(cell)
+            notify_self("Finished {}".format(args.message))
+        except BaseException as e:
+            notify_self("Error while {}: {}".format(args.message, e))
+            raise e
+
+ip = get_ipython()
+ip.register_magics(MessengerMagics)
