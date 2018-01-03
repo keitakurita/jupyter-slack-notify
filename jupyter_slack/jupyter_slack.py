a09657adf1db70aa7074744c264b4a57d9347d70
keitakurita <keita.kurita@gmail.com>
2018-01-03 13:07:00 +0900

    NEW: Added time measurement feature

--- jupyter_slack/jupyter_slack.py
+++ jupyter_slack/jupyter_slack.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import time
 from IPython.core import magic_arguments
 from IPython.core.magics import ExecutionMagics
 from IPython.core.magic import cell_magic, magics_class
@@ -17,6 +18,26 @@ def notify_self(message):
     return r.text
 
 
+def construct_time_mess(elapsed):
+    day = elapsed // (24 * 3600)
+    elapsed = elapsed % (24 * 3600)
+    hour = elapsed // 3600
+    elapsed %= 3600
+    minutes = elapsed // 60
+    elapsed %= 60
+    seconds = round(elapsed, 1)
+    time_mess = ""
+    if day > 0:
+        time_mess += " {} days".format(day)
+    if hour > 0:
+        time_mess += " {} hours ".format(hour)
+    if minutes > 0:
+        time_mess += " {} minutes".format(minutes)
+    if seconds > 0:
+        time_mess += " {} seconds".format(seconds)
+    return time_mess
+
+
 @magics_class
 class MessengerMagics(ExecutionMagics):
 
@@ -26,11 +47,17 @@ class MessengerMagics(ExecutionMagics):
     @cell_magic
     @magic_arguments.magic_arguments()
     @magic_arguments.argument("message", type=str)
+    @magic_arguments.argument("--time", "-t", action="store_true")
     def notify(self, line="", cell=None):
         args = magic_arguments.parse_argstring(self.notify, line)
         mess = args.message.replace("\"", "")
+        start = time.time()
         try:
             self.shell.ex(cell)
+            if args.time:
+                elapsed = time.time() - start
+                time_mess = construct_time_mess(elapsed)
+                mess += " in" + time_mess
             notify_self("Finished {}".format(mess))
         except BaseException as e:
             notify_self("Error while {}: {}".format(mess, e.__repr__()))
