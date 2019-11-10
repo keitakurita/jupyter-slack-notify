880e7f76321e5ad4712ddcc52dfd6e0ffdb85b04
keitakurita <keita.kurita@gmail.com>
2019-11-09 20:04:42 -0500

    Debugged option to show entire traceback

--- tests/test_notify.py
+++ tests/test_notify.py
@@ -12,7 +12,18 @@ def test_context_manager_no_exception_time():
     with jupyter_slack.Monitor("test no exception timed", time=True):
         pass
 
-def test_context_manager_exceptin():
+def test_context_manager_exception():
     with pytest.raises(ValueError, match="AAAAA"):
         with jupyter_slack.Monitor("test exception", time=False):
             raise ValueError("AAAAA")
+
+def test_context_manager_exception_tb():
+    def external_fn():
+        raise ValueError("AAAAA")
+    def error_fn():
+        def inner_error_fn():
+            external_fn()
+        inner_error_fn()
+    with pytest.raises(ValueError):
+        with jupyter_slack.Monitor("test exception", time=False, send_full_traceback=True):
+            error_fn()
